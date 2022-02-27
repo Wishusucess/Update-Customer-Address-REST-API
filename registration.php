6d1021238039c8af2eb47e4929f5674d59fd1c20
Wishusucess <90965189+Wishusucess@users.noreply.github.com>
2022-02-27 05:56:13 +0530

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/*
+* Developer: Hemant Singh Magento 2x Developer
+* Website:   http://www.wishusucess.com/
+*/    
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Wishusucess_UpdateAddress',
+    __DIR__
+);
